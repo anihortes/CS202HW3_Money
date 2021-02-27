0f8e07c9007e8e242a028a06b3549172e29a28b0
Adam Owen <ahowenjr@alaska.edu>
2021-02-26 20:56:51 -0900

    done. had to rework code to fit the moneytests standards

--- Money.h
+++ Money.h
@@ -1,7 +1,6 @@
 //
 // Created by A. Harrison Owen on 2/24/2021.
 //
-#include <ostream>
 #ifndef CS202HW3_MONEY_MONEY_H
 #define CS202HW3_MONEY_MONEY_H
 
@@ -10,8 +9,12 @@ class Money {
     friend std::ostream & operator<<(std::ostream &, const Money &);
     friend Money operator+(const Money &lhs, const Money &rhs);
     friend Money operator-(const Money &lhs, const Money &rhs);
+    friend bool operator==(const Money &lhs, const Money &rhs);
+    friend bool operator<(const Money &lhs, const Money &rhs);
+
 public:
     Money();
+    Money(double);
     Money(int, int);
     ~Money();
 
@@ -21,15 +24,24 @@ public:
     Money operator-() const;
     Money & operator+=(const Money &rhs);
     Money & operator-=(const Money &rhs);
-    Money & operator*=(const Money &rhs);
-    Money & operator/=(const Money & rhs);
-
-
+    Money & operator*=(const double &a);
+    Money & operator/=(const double &a);
+    Money & operator++();
+    Money & operator++(int);
+    Money & operator--();
+    Money & operator--(int);
+    
 private:
     int _dollars;
     int _cents;
     double _value;
 };
-Money operator*(Money lhs, const Money &rhs);
+Money operator*(Money lhs, const double &a);
+Money operator*(const double &a, Money rhs);
+Money operator/(Money lhs, const double &a);
 
+bool operator!=(const Money &lhs, const Money &rhs);
+bool operator>(const Money &lhs, const Money &rhs);
+bool operator<=(const Money &lhs, const Money &rhs);
+bool operator>=(const Money &lhs, const Money &rhs);
 #endif //CS202HW3_MONEY_MONEY_H
